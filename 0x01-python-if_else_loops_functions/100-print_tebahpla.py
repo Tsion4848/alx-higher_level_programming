7c62d5d97b0fc07f70068e437141511b5e5b1b01
Tsion Moges <tsionmoges25@gmail.com>
2022-06-01 05:46:45 +0300

    upper and lowercases

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for x in "zyxwvutsrqponmlkjhgfedcba":
-    if ord(x) >= 97 and ord(x) <= 122 || ord(x) >= 65 && ord(x) <= 90:
+    if ord(x) >= 97 and ord(x) <= 122:
         y = ord(x)
-        print("{:s}".format(chr(y)), end="")
         y = y + 1
         y = ord(x) - 32
     else:
         y = ord(x)
+    print("{:s}".format(chr(y)), end="")
