812878b249ba81f1a4eead18ca70551c707c7fbb
Tsion Moges <tsionmoges25@gmail.com>
2022-06-01 04:45:53 +0300

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizz", end=" ")
         elif x % 5 == 0:
             print("Buzz", end=" ")
-        elif x % 3 == 0 and x % 5 == 0:
+        if x % 3 == 0 and x % 5 == 0:
             print("FizzBuzz", end=" ")
         else:
             print("{:d}".format(x), end=" ")
