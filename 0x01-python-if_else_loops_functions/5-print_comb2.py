cdaa86b4caeeac5dac788cba130565af29e363e0
Tsion Moges <tsionmoges25@gmail.com>
2022-06-01 03:00:04 +0300

    printing numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if x < 10:
-        print("0{:d}".format(x), end=", ")
-    elif x != 99:
-        print("{:d}".format(x), end=", ")
+    if x != 99:
+        print("{:02d}".format(x), end=", ")
+    else:
+        print("{:d}".format(x))
