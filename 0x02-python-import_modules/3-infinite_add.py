9ef030f176d7930235313feb43a851a5a82748ba
Tsion Moges <tsionmoges25@gmail.com>
2022-06-02 23:54:16 +0300

    adding args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     from sys import argv
     summ = 0
     for x in range(1, len(argv)):
-        summ = summ + int(argv[x])
-    print("{:d}".format(summ)))
+        summ = summ + int(x)
+    print("{:d}".format(summ))
