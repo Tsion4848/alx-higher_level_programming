6007045e9e377339e60d2cbc3ca1c4b806918573
Tsion Moges <tsionmoges25@gmail.com>
2022-06-02 22:29:20 +0300

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-if len(argv) == 1:
-    print("{:d} argument: {:s}".format(len(argv), argv))
-elif len(argv) > 1:
-    print("{:d} arguments: {:s}".format(len(argv), argv))
-elif len(argv) == 0:
-    print("{:d} arguments.".format(len(argv))
+if __name == '__main__';
+    from sys import argv
+    if len(argv) == 1:
+        print("0 arguments")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {:s}".format(argv[1]))
+    elif len(argv) > 2:
+        print("{:d} arguments:".format(len(argv)))
+        for x in range (1, len(argv)):
+            print("{:d}: {s}".format(arg, argv[x]))
