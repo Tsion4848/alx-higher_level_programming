958259723d00b0e3cee69976d8695fe1610b60b9
Tsion Moges <tsionmoges25@gmail.com>
2022-06-02 23:15:44 +0300

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
         print("1: {:s}".format(argv[1]))
     elif len(argv) > 2:
         print("{:d} arguments:".format(len(argv) - 1))
-        for x in range (1, len(argv)):
+        for x in range(1, len(argv)):
             print("{:d}: {:s}".format(x, argv[x]))
