ad0d44430fd10b1b354c2d19a328b20c68839335
Tsion Moges <tsionmoges25@gmail.com>
2022-06-03 01:02:14 +0300

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1.py import add, sub, mul, div
+    from sys import argv
+    if len(argv) == 4:
+        a = int(argv[1])
+        b = int(argv[3])
+        y = argv[2]
+        oper = {'+': add, '-': sub, '*': mul, '/': div}
+        for x in oper:
+            if y == x:
+                print("{} {} {} = {}".format(a, y, b, oper[x](a, b)))
+                exit(0)
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
