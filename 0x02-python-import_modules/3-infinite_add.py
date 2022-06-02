842060f0a79916a5665803e473f05b6dd0d394df
Tsion Moges <tsionmoges25@gmail.com>
2022-06-02 23:58:42 +0300

    sum of args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == '__main__':
     from sys import argv
     summ = 0
-    for x in range(1, len(argv) + 1):
+    for x in argv[1:]:
         summ = summ + int(x)
     print("{:d}".format(summ))
