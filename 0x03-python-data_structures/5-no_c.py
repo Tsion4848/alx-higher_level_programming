d4431094589c5842a5517c40f368aebe7f66f52f
Tsion Moges <tsionmoges25@gmail.com>
2022-06-06 19:50:58 +0300

    remove c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     copy = ""
-    for x in my_string:
+    for x in range(0. len(my_string)):
         if my_string[x] != 'c' and my_string[x] != 'C':
             copy = copy + my_string[x]
         return copy
