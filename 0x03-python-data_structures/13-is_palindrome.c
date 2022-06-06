3bc851d653476ed25179d2e4d53c700b1faffd43
Tsion Moges <tsionmoges25@gmail.com>
2022-06-06 22:10:17 +0300

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * is_palindrome - checkes whether a singly-linked
+ * list is palindrome or not
+ *
+ * @head: the pointer 
+ *
+ * Return: 1 if palindrome or 0 if not
+ */
