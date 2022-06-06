b64615fabb80846c1c4cfa1a5dcb4be36923e6a6
Tsion Moges <tsionmoges25@gmail.com>
2022-06-06 20:28:03 +0300

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     for x in my_list:
         if x % 2 == 0:
-            copy[x] = True;
+            copy = True;
         else:
-            copy[x] = False;
+            copy = False;
     return copy
