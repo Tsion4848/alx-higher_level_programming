54a488e09715866a77aee483773b3c31e927dfb2
Tsion Moges <tsionmoges25@gmail.com>
2022-06-06 19:30:05 +0300

    replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    for x in range(len(my_list) - 1 , 0):
-        print("{:d}".format(my_list[x]))
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
