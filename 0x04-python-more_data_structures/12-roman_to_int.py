aaf14c2f8c9d7867eced1e38b408012e05733c76
Tsion Moges <tsionmoges25@gmail.com>
2022-06-09 17:27:47 +0300

    roman to string

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if is_instance(roman_string, str):
+        res = 0
+        change = 0
+        dictnums = {'I': 1, 'v': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+        for x in reversed(roman_string):
+            change = dictnums[x]
+            if result < change * 5:
+                res = res + change
+            else:
+                res = res - change
+            return res
+    else:
+        return 0
