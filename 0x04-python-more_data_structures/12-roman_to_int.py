7cd7a72c750c7c6af39ad34c92fa6b3105f3fc86
Tsion Moges <tsionmoges25@gmail.com>
2022-06-09 17:29:19 +0300

    roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if is_instance(roman_string, str):
+    if isinstance(roman_string, str):
         res = 0
         change = 0
         dictnums = {'I': 1, 'v': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
