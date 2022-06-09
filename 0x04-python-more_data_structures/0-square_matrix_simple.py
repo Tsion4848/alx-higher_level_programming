6221201c160e61ef3efb8924ba4e42b541b3b7cb
Tsion Moges <tsionmoges25@gmail.com>
2022-06-09 15:07:44 +0300

    updated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 def square_matrix_simple(matrix=[]):
     squares = []
-    for x in range(len(matrix)):
-        squares.append(x**2)
+    for x in range(0, len(matrix)):
+        squares[x] = matrix[x] ** 2
     return squares
