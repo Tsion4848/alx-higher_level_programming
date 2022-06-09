98864663e91686d9e6d7826c10f69ae585d93689
Tsion Moges <tsionmoges25@gmail.com>
2022-06-09 15:55:21 +0300

    add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
+    add = 0
     for x in my_list:
         if my_list.count(x) == 1:
-            sum = sum + x
+            add= add + x
         else:
-            sum = sum + 0
-    return sum
+            add = add + 0
+    return add
