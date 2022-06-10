4e6f2488178798becf01c19c33fe536c46423333
Tsion Moges <tsionmoges25@gmail.com>
2022-06-10 20:27:46 +0300

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    num, den = 0,0
+    num, den = 0, 0
     if (my_list and len(my_list) > 0):
         for (score, weight) in my_list:
             num += score * weight
