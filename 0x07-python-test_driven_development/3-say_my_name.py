d99cc82aa4812e372dd5c041c119f3856be44360
Tsion Moges <tsionmoges25@gmail.com>
2022-07-01 14:20:58 +0300

    prints name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+say_my_name function
+This function prints "My name is <first_name> <last_name>"
+"""
+def say_my_name(first_name, last_name=""):
+    if type(first_name) not in str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) not in str:
+        raise TypeError('last_name must be a string')
+    print('My name is {} {}'.format(first_name, last_name))
