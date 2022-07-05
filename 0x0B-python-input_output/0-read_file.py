d4be6980d34ca573959159143bce5e221d6b640d
Tsion Moges <tsionmoges25@gmail.com>
2022-07-05 18:14:59 +0300

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open('filename', 'r', encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         data = f.read()
+    print("{:s}".format(data, end=""))
     f.close()
-    print("{:s}".format(data))
