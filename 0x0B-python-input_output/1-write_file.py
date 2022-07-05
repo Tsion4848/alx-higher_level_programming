5933ef9a5b4db25eb8ec0f5af888271af3ed1302
Tsion Moges <tsionmoges25@gmail.com>
2022-07-05 18:23:42 +0300

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
-    with open(filename, 'w', encoding = "utf-8"):
-        f.write(text)
-    f.close()
+    """
+    Writes a string to a text file (UTF8) and returns
+    the number of characters written:
+    Prototype: def write_file(filename="", text=""):
+    Create the file if doesn’t exist and overwrite the content
+    of the file if it already exists.
+    """
+    with open(filename, 'w', encoding = "utf-8") as f:
+        return (f.write(text))
