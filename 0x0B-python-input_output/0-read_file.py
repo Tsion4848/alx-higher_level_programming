9acc2ad7833cc9ad790b6ca6d5d02944dd4f9845
Tsion Moges <tsionmoges25@gmail.com>
2022-07-07 03:26:58 +0300

    up

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+__import__("my_module").__doc__
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout
@@ -6,4 +7,3 @@ def read_file(filename=""):
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
     f.close()
-    print(__import__("my_module").__doc__)
