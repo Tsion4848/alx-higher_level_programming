37a92617e7202830784e303c68a0dbfad9269dc6
Tsion Moges <tsionmoges25@gmail.com>
2022-09-06 21:55:20 +0300

    printing x in rectangular format

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,14 +9,10 @@ module.exports = class Rectangle {
     }
   }
 
-
   print () {
     let y = 'X';
-    for (let i = 0 ; i < this.height ; i++) {
-      for (let j = 0 ; j < this.width ; j++) {
-        console.log(y);
-      }
-      console.log("\n");
+    for (let i = 0; i < this.height; i++) {
+      console.log(y.repeat(this.width));
     }
   }
 };
