2d9ab514a06a8b1b4ace0c207a7a954c44d59de0
Tsion Moges <tsionmoges25@gmail.com>
2022-09-06 21:35:14 +0300

    creating empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
       object.create(Rectangle);
-    } else {
+    } else if (w && h) {
       this.width = w;
       this.height = h;
     }
