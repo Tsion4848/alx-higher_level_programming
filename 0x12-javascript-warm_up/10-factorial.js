2336ad1067150b10da07af0bcdaf90cdaa714b56
Tsion Moges <tsionmoges25@gmail.com>
2022-09-06 07:31:45 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 function factorial (x) {
-	if (isNaN(n) === true || n ===0) {
+	if (isNaN(x) === true || x === 0) {
 		return (1);
 	}
-	return (n * factorial(n - 1));
+	return (x * factorial(x - 1));
 }
-console.log(factorial(parseInt(process.argv[2],10)));
+console.log(factorial(parseInt(process.argv[2], 10)));
