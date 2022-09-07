50ff353ce87279c130b6f31f647cd98d2d640cd8
Tsion Moges <tsionmoges25@gmail.com>
2022-09-07 06:10:00 +0300

    print c in rectangular format

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square') {
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
 	    c = 'X';
     }
-    for (int i = 0 ; i < this.height ; i++) {
+    for (let i = 0 ; i < this.height ; i++) {
       console.log(c.repeat(this.width));
     }
   }
