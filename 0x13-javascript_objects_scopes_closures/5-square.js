ba4be9da458ac452a3703df0ddbb7054aca1f64a
Tsion Moges <tsionmoges25@gmail.com>
2022-09-07 05:59:04 +0300

    square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      Object.create(Rectangle);
+    } else if (w && h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let y = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(y.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let x = this.width;
+    this.width = this.height;
+    this.height = x;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = class square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
