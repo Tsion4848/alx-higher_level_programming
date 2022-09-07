7e77a0da72ddecaed850f50ea9715ccbb3c9d044
Tsion Moges <tsionmoges25@gmail.com>
2022-09-07 06:33:32 +0300

    sort

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,7 +4,7 @@ let old = require('./101-data.js').dict;
 let newDic = {};
 Object.keys(old).forEach(function (key) {
   if (newDic[old[key]] === undefined) {
-    new[old[key]] = [];
+    newDic[old[key]] = [];
   }
   newDic[old[key]].push(key);
 });
