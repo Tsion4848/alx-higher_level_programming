a8e3578f769e88a6703ce63347344c643534f0e6
Tsion Moges <tsionmoges25@gmail.com>
2022-09-23 21:35:19 +0300

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# a python script that fetches a url
+"""a python script that fetches a url"""
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+
+if __name__ == "__main__":
+    """fetches a url"""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
+    html_dec = html.decode('utf-8')
